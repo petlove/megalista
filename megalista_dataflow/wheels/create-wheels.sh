f4652e6936358c1178aba03cd3c9bf3a8fb6c763
Caio Tomazelli <caiotomazelli@google.com>
2022-07-14 13:29:51 -0300

    Update create-wheels script to run python3

--- megalista_dataflow/wheels/create-wheels.sh
+++ megalista_dataflow/wheels/create-wheels.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip wheel -r ./wheels/requirements.txt -w ./wheels/packages
+python3 -m pip wheel -r ./wheels/requirements.txt -w ./wheels/packages
 #pip wheel --no-deps -r ./megalista_dataflow/requirements-no-deps.txt -w ./wheels/packages
